10f90d81745fc0bffda08a76e6359febad69bf0b
Emiron <emartinsmiron@gmail.com>
2023-02-06 15:24:54 -0300

    Criando evento específico para troca de endereço e alterando testes

--- src/domain/customer/event/handler/notify-address-change.event.ts
+++ src/domain/customer/event/handler/notify-address-change.event.ts
@@ -1,6 +1,7 @@
+import CustomerChangeAddressEvent from "../customer-change-address.event";
 import CustomerCreatedEvent from "../customer-created.event";
 
-export default class NotifyAddressChangeEvent implements EventHandlerInterface<CustomerCreatedEvent>{
+export default class NotifyAddressChangeEvent implements EventHandlerInterface<CustomerChangeAddressEvent>{
     handle(event: CustomerCreatedEvent): void {
        console.log("Endereço do cliente: %s, %s alterado para: %s", event.eventData.id, event.eventData.name, event.eventData.address.street);
     }
